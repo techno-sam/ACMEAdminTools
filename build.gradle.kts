efd35c0cd8b8b99f7a215a080e7cf0bdd0dc6a6c
techno-sam <77073745+techno-sam@users.noreply.github.com>
2024-10-03 09:07:15 -0700

    finish setting up mod

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 import java.io.ByteArrayOutputStream
+import dev.ithundxr.silk.ChangelogText
 
 plugins {
     java
@@ -23,18 +24,24 @@ val build = buildNumber?.let { "-build.${it}" } ?: "-local"
 
 version = "${"mod_version"()}+${project.name}-mc${"minecraft_version"() + if (isRelease) "" else build}"
 
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-}
-
 java {
     withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release = 17
 }
 
 repositories {
     mavenCentral()
     exclusiveMaven("https://maven.parchmentmc.org", "org.parchmentmc.data") // Parchment mappings
-    exclusiveMaven("https://maven.quiltmc.org/repository/release", "org.quiltmc") // Quilt Mappings
+    maven("https://maven.terraformersmc.com/releases/") // Mod Menu, EMI
+    maven("https://jitpack.io/") // Mixin Extras, Fabric ASM
+    exclusiveMaven("https://api.modrinth.com/maven", "maven.modrinth") // LazyDFU
 }
 
 val loom = project.extensions.getByType<LoomGradleExtensionAPI>()
@@ -43,6 +50,94 @@ loom.apply {
         vmArg("-XX:+AllowEnhancedClassRedefinition")
         vmArg("-XX:+IgnoreUnrecognizedVMOptions")
         vmArg("-Dmixin.debug.export=true")
+        vmArg("-Dmixin.env.remapRefMap=true")
+        vmArg("-Dmixin.env.refMapRemappingFile=${projectDir}/build/createSrgToMcp/output.srg")
+    }
+}
+
+loom {
+    runs {
+        create("datagen") {
+            client()
+
+            name = "Minecraft Data"
+            vmArg("-Dfabric-api.datagen")
+            vmArg("-Dfabric-api.datagen.output-dir=${project.file("src/generated/resources")}")
+            vmArg("-Dfabric-api.datagen.modid=acme_admin")
+            vmArg("-Dporting_lib.datagen.existing_resources=${project.file("src/main/resources")}") // if we add porting lib in the future
+
+            environmentVariable("DATAGEN", "TRUE")
+        }
+    }
+}
+
+configurations.configureEach {
+    resolutionStrategy {
+        force("net.fabricmc:fabric-loader:${"fabric_loader_version"()}")
+    }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${"minecraft_version"()}")
+    // layered mappings - Mojmap names, parchment and QM docs and parameters
+    @Suppress("UnstableApiUsage")
+    mappings(loom.layered {
+        officialMojangMappings { nameSyntheticMembers = false }
+        parchment("org.parchmentmc.data:parchment-${"minecraft_version"()}:${"parchment_version"()}@zip")
+    })
+
+    // Used to decompile mixin dumps, needs to be on the classpath
+    // Uncomment if you want it to decompile mixin exports, beware it has very verbose logging.
+    //implementation("org.vineflower:vineflower:1.10.0")
+
+    modImplementation("net.fabricmc:fabric-loader:${"fabric_loader_version"()}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${"fabric_api_version"()}")
+
+    // Development QOL
+    modLocalRuntime("maven.modrinth:lazydfu:${"lazydfu_version"()}")
+    modLocalRuntime("com.terraformersmc:modmenu:${"modmenu_version"()}")
+
+    modLocalRuntime("dev.emi:emi-fabric:${"emi_version"()}")
+
+    annotationProcessor(implementation("io.github.llamalad7:mixinextras-common:${"mixin_extras_version"()}")!!)
+}
+
+tasks.processResources {
+    val properties = mapOf(
+        "version" to version,
+        "minecraft_version" to "minecraft_version"(),
+        "fabric_api_version" to "fabric_api_version"(),
+        "fabric_loader_version" to "fabric_loader_version"(),
+    )
+
+    inputs.properties(properties)
+
+    filesMatching("fabric.mod.json") {
+        expand(properties)
+    }
+
+    // don't add development or to-do files into built jar
+    exclude("**/*.bbmodel", "**/*.lnk", "**/*.xcf", "**/*.md", "**/*.txt", "**/*.blend", "**/*.blend1")
+}
+
+sourceSets.main {
+    resources { // include generated resources in resources
+        srcDir("src/generated/resources")
+        exclude(".cache/**")
+    }
+}
+
+tasks.jar {
+    archiveClassifier = "dev"
+
+    manifest {
+        attributes(mapOf("Git-Hash" to gitHash))
+    }
+}
+
+tasks.named<Jar>("sourcesJar") {
+    manifest {
+        attributes(mapOf("Git-Hash" to gitHash))
     }
 }
 
@@ -80,5 +175,21 @@ fun RepositoryHandler.exclusiveMaven(url: String, vararg groups: String) {
                 includeGroup(it)
             }
         }
+    }
+}
+
+publishMods {
+    file = tasks.remapJar.get().archiveFile
+    version.set(project.version.toString())
+    changelog = ChangelogText.getChangelogText(rootProject).toString()
+    type = STABLE
+    displayName = "ACME Admin Tools v${"mod_version"()} Fabric ${"minecraft_version"()}"
+    modLoaders.add("fabric")
+    modLoaders.add("quilt")
+
+    modrinth {
+        projectId = "modrinth_id"()
+        accessToken = System.getenv("MODRINTH_TOKEN")
+        minecraftVersions.add("minecraft_version"())
     }
 }
--- src/main/java/io/github/slimeistdev/acme_admin/ACMEAdminTools.java
+++ src/main/java/io/github/slimeistdev/acme_admin/ACMEAdminTools.java
@@ -19,6 +19,6 @@ public class ACMEAdminTools implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("ACME Admin Tools is loading!");
 	}
 }
